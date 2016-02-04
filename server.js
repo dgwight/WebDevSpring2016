b717991e6f35956a165a0db0938ba79828442e80
Dylan Wight <dylangwight@gmail.com>
2016-02-03 21:49:29 -0500

    trying different server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/assingment/home.html'));
-});var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.use(express.static(__dirname + '/public'));
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.listen(port, ipaddress);
